46e2ae313f79f795ecee4053552e051f37bfad8f
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-09-25 00:53:27 +0300

    Create Greating.js  component

--- src/Greeting.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-
-function Greeting() {
-  const greeting = useSelector((state) => state.greeting);
-  return <div>{greeting}</div>;
-}
-
-export default Greeting;
